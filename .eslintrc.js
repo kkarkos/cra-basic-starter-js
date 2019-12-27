d4969b88ae6a3342cf80ee95102847d1d839d98d
Konrad <konrad@krad.rocks>
2019-12-27 19:28:10 +1100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['@airteam'],
+  rules: {
+    'jsx-a11y/no-onchange': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+  },
+};
